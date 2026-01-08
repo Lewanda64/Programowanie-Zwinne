181f4c4958cf886ff44f0d3aaf955818d9d832ed
k2extreme <k2mil@poczta.onet.pl>
2026-01-08 02:33:40 +0100

    Naprawa skanowania pakietów (com.project) i włączenie JPA repozytoriów/encji; frontend serwowany ze src/main/resources/static

---
+++ src/main/java/com/project/project_rest_api/ProjectRestApiApplication.java
@@ -0,0 +1,19 @@
+package com.project.project_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+
+@EnableJpaAuditing
+@SpringBootApplication(scanBasePackages = {"com.project", "com.project.project_rest_api"})
+@EnableJpaRepositories(basePackages = "com.project.repository")
+@EntityScan(basePackages = "com.project.model")
+public class ProjectRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRestApiApplication.class, args);
+	}
+
+}
